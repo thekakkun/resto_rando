f499ceaf9632eb208b5a5c3334b3fb73d69ca18a
Hiroto Kaku <20251225+thekakkun@users.noreply.github.com>
2022-03-28 11:55:50 -0400

    Create basic flask app

---
+++ flaskr/__init__.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_migrate import Migrate
+import os
+
+from .models import db
+
+DB_USER = os.environ.get('DB_USER')
+DB_PASSWORD = os.environ.get('DB_PASSWORD')
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgres://{DB_USER}:{DB_PASSWORD}@localhost:5432/resto_rando'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+
+migrate = Migrate(app, db)
+
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
